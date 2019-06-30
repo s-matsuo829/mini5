53ef290a617406d41157993c899043a9f22308e0
s-matsu829 <fakefur829@gmail.com>
2019-06-30 21:19:52 +0900

    mini5 add

---
+++ app/assets/javascripts/clock.js
@@ -0,0 +1,9 @@
+function showClock1() {
+  var nowTime = new Date();
+  var nowHour = nowTime.getHours();
+  var nowMin  = nowTime.getMinutes();
+  var nowSec  = nowTime.getSeconds();
+  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("RealtimeClockArea").innerHTML = msg;
+}
+setInterval('showClock1()',1000);
